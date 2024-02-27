f68c0606b56bd12f94471e1630c12e0eaa359004
CATHAL COMMINS - STUDENT <g00427615@atu.ie>
2024-02-27 16:56:47 +0000

    Setup rounds and printing board

--- main.c
+++ main.c
@@ -1,11 +1,42 @@
 #include <stdio.h>
 
-char board[3][3] = { {'O', ' ', ' '}, {'X', 'O', ' '}, {'X', ' ', 'O'} };
+char board[3][3] = { {'X', ' ', ' '}, {'X', 'O', ' '}, {'X', ' ', 'O'} };
 
 void printboard();
 
 int main() {
-    printboard();
+    char CurrentGo = 'X', UserName[50];
+    int y, x;
+
+
+    //Welcome User and get user name
+    printf("Welcome to X's and O's. Please enter your username: ");
+    fgets(UserName, sizeof(UserName), stdin);
+
+    //Remove Newline character from UserName
+    for (int i = 0; i != sizeof(UserName); i++) {
+        if (UserName[i] == '\n') {
+            UserName[i] = '\0';
+            break;
+        }
+    }
+
+    //For loop for game. Will only run 9 times
+    for (int i = 0; i != 9; i++) {
+        if (CurrentGo == 'X') {
+            printf("Its your go now %s, Please enter your go.\n", UserName);
+
+            printf("Enter a letter: ");
+            scanf_s("%i", &x);
+
+            printf("Enter a Number: ");
+            scanf_s("%i", &y);
+
+            printf("%i",x);
+        }
+
+        
+    }
 }
 
 void printboard() {
